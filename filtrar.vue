7b55fdab0dc8c0f65b07d9180ce0c14c1cea6cea
jaquemoura <49768265+jaquemoura@users.noreply.github.com>
2023-10-14 20:20:30 -0300

    Create filtrar.vue

---
+++ filtrar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div>
+            <slot name="titulo"></slot>
+        </div>
+        <input v-model="pesquisaInput" type="text">
+
+        <ul>
+            <template>
+                <li v-for="(opcao, i) in opcoesAtiva" :key="i">{{opcao.nome}}</li>
+            </template>
+        </ul>
+
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        opcoesFiltro: Array,
+    },
+    data() {
+        return {
+            pesquisaInput: "",  
+        }
+    },
+    computed: {
+        opcoesAtiva() {
+            // let filtros;
+
+            return this.opcoesFiltro.filter(opcao => {
+            return opcao.nome.toLowerCase().includes(this.pesquisaInput.toLowerCase());
+            });
+
+            // return filtros;
+        }
+    }
+}
+</script>
